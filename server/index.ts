35836078264800ed96d2d01fd448e7e8dc45e89f
Tobias Sassin <tobi.sassin@gmail.com>
2024-03-06 19:01:34 +0100

    improve console output

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,20 @@
 import { Server } from 'socket.io'
 import { ServerState, Room, Card } from './types'
+import { v4 as uuidv4 } from 'uuid'
 
 const express = require('express')
 const http = require('http')
 const app = express()
 const server = http.createServer(app)
-const port = process.env.SOCKET_IO_PORT || 3001
-console.log("env", process.env.SOCKET_IO_PORT);
+const port = 3001
+const runtimeId = uuidv4(); // for debugging purposes and to check if the server has restarted
 
 const io = new Server(server, {
     cors: {
         origin: '*',
     },
 })
-
+console.log("LOG: server started \n'-> runtimeId:", runtimeId)
 
 let serverState: ServerState = {
     rooms: {}
@@ -24,7 +25,6 @@ serverState.rooms['room1'] = {
 } as unknown as Room;
 
 const handleCardSelect = (card: Card, roomId: string) => {
-    console.log('handleCardSelect', card, roomId)
     if (serverState.rooms[roomId]) {
         const existingCardIndex = serverState.rooms[roomId].cards.findIndex(c => c.placedBy === card.placedBy)
         if (existingCardIndex !== -1) {
@@ -33,29 +33,34 @@ const handleCardSelect = (card: Card, roomId: string) => {
             if (card.id === null || card.id === '') {
                 // remove the card from the list
                 serverState.rooms[roomId].cards.splice(existingCardIndex, 1);
+                console.debug('DBG:handleCardSelect - card removed - roomId:', roomId, "card:", card);
             } else {
                 // update the card
                 serverState.rooms[roomId].cards[existingCardIndex] = card;
+                console.debug('DBG:handleCardSelect - card updated - roomId:', roomId, "card:", card);
             }
         } else {
             // user has not placed a card yet, add it
             if (card.id === null || card.id === '') return; // no card to add
             serverState.rooms[roomId].cards.push(card);
+            console.debug('DBG:handleCardSelect - card added - roomId:', roomId, "card:", card);
         }
         io.to(roomId).emit('cardsUpdated', serverState.rooms[roomId].cards);
-        console.log('cardsUpdated', serverState.rooms[roomId].cards);
+        console.log('LOG:handleCardSelect - card placed - roomId:', roomId, "card:", card);
+        return;
     } 
+    console.error('ERR:handleCardSelect - room not found - roomId:', roomId);
 }
 
 
-
 io.on('connection', (socket) => {
-    console.log('a user connected')
+    console.debug('VEB: a user connected')
 
     socket.on('createRoom', (roomId: string, userId: string, roomName?: string) => {
-        console.log('createRoom', roomId, userId)
+        console.log('LOG:createRoom - roomId:', roomId, "userId:", userId)
         if (serverState.rooms[roomId]) {
-            socket.emit('roomExists', roomId)
+            socket.emit('roomExists', roomId);
+            console.error('ERR:createRoom - roomExists roomId:', roomId);
         } else {
             serverState.rooms[roomId] = {
                 id: roomId,
@@ -65,24 +70,28 @@ io.on('connection', (socket) => {
                 cards: [],
                 cardsVisible: false
             }
-            socket.emit('roomCreated', roomId)
-            socket.join(roomId)
+            socket.emit('roomCreated', roomId);
+            socket.join(roomId);
         }
     })
 
     socket.on('joinRoom', (roomId: string) => {
-        console.log('joinRoom', roomId)
 
         if (!serverState.rooms[roomId]) {
-            socket.emit('roomNotFound', roomId)
+            socket.emit('roomNotFound', roomId) 
+            console.debug('DBG:joinRoom - roomNotFound roomId:', roomId)
         } else {
             socket.join(roomId)
             socket.emit('roomJoined', roomId)
+            io.to(roomId).emit('cardsUpdated', serverState.rooms[roomId].cards);
+            console.debug('DBG:joinRoom - roomJoined roomId:', roomId)
         }
     })
 
+// TODO save runtimeId in token and check if it matches 
+// (when server restarts placing a card will not work anymore because the room does not exist anymore)
     socket.on("placeCard", (cardName: string, roomId: string, userId: string) => {
-        console.log('placeCard', cardName, roomId, userId)
+        console.debug('DBG:placeCard - cardName:', cardName, "roomId:", roomId, "userId:", userId)
         const card: Card = {
             id: cardName,
             placedBy: userId
@@ -97,10 +106,10 @@ io.on('connection', (socket) => {
 
 
     socket.on('disconnect', () => {
-        console.log('user disconnected')
+        console.debug('VEB: user disconnected')
     })
 })
 
 server.listen(port, () => {
-    console.log(`listening on *:${port}`)
+    console.log(`LOG: listening on *:${port}`)
 })
