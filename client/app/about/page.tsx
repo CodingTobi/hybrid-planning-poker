c311f4abfc771971926e386219cd2e081c150a8c
Tobias Sassin <tobi.sassin@gmail.com>
2024-03-06 19:03:43 +0100

    Add About and Privacy pages

---
+++ client/app/about/page.tsx
@@ -0,0 +1,19 @@
+const About = () => {
+    return (
+        <div className="container mx-auto px-4 py-8">
+
+            <h1 className="text-3xl font-bold mb-4 mt-24 text-center">Über diese Arbeit</h1>
+
+            <div className="bg-white p-6 rounded-lg shadow-md">
+
+                <p className="text-lg mb-4">Wilkommen bei meiner Bachelorarbeit</p>
+
+                <p className="mb-4">
+                    Spätestens seit den präsenzbeschränkenden Maßnahmen während der Corona-Pandemie haben sich in verschiedensten Bereichen des Lebens online Lösungen durchgesetzt. Auch in der Lehre sind viele Prozesse digitalisiert worden und auch teilweise digital geblieben. Da Treffen in Person ebenfalls Vorteile haben, sind diese nach Wegfall der Maßnahmen weiterhin sinnvoll. Bei dem Versuch, das Beste aus beiden Welten zu nutzen, entstehen teilweise hybride Lösungen, die sowohl online als auch in Präsenz funktionieren. Auch Studenten der HSRM haben bereits die Vorteile von hybriden Vorlesungen erfahren können, bei denen vor Ort oder von Zuhause aus zugesehen werden kann. So muss ein Student, der weiter weg wohnt, nicht unbedingt für eine einzelne Vorlesung bis zum Campus fahren, aber Studenten, die keine gute Internetanbindung haben oder sich vor Ort besser konzentrieren können, haben weiterhin die Möglichkeit in Präsenz zu erscheinen.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default About;
---
+++ client/app/privacy/page.tsx
@@ -0,0 +1,29 @@
+const Privacy = () => {
+    return (
+        <div className="container mx-auto p-8 mt-24">
+            <h1 className="text-3xl font-bold mb-4 text-center">Impressum</h1>
+            <div className="bg-white p-6 rounded-lg shadow-md">
+                <p>Bachelorarbeit im Rahmen der</p>
+                <p><strong>Hochschule RheinMain</strong></p>
+                <p>Kurt-Schumacher-Ring 18</p>
+                <p>65197 Wiesbaden</p>
+                <p>Deutschland</p>
+                <p><strong>Vertreten durch:</strong></p>
+                <p>Präsidentin: Prof. Dr. Eva Waller</p>
+                <p><strong>Kontakt:</strong></p>
+                <p>E-Mail: <a href="mailto:praesidiumssekretariat@hs-rm.de">praesidiumssekretariat(at)hs-rm.de</a></p>
+                <p><strong>Umsatzsteueridentifikationsnummer:</strong> DE811490955</p>
+                <p><strong>Aufsichtsbehörde:</strong></p>
+                <p>Hessisches Ministerium für Wissenschaft und Kunst</p>
+                <p>Rheinstraße 23 - 25</p>
+                <p>65185 Wiesbaden</p>
+                <p>Deutschland</p>
+                <p><strong>Haftungshinweis:</strong></p>
+                <p>Die Hochschule RheinMain übernimmt keine Haftung für die Inhalte externer Links. Für den Inhalt der
+                    verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Privacy;
