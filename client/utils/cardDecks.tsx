445851d93a7bc501ba1b5603f93c15ebc7882dc1
Tobias Sassin <tobi.sassin@gmail.com>
2024-03-07 22:02:22 +0100

    Refactor Test component to use default card deck

---
+++ client/utils/cardDecks.tsx
@@ -0,0 +1,14 @@
+export const cards_default = [ 
+    { cardName: 'card_0', cardValue: '0' },
+    { cardName: 'card_1', cardValue: '1' },
+    { cardName: 'card_2', cardValue: '2' },
+    { cardName: 'card_3', cardValue: '3' },
+    { cardName: 'card_5', cardValue: '5' },
+    { cardName: 'card_8', cardValue: '8' },
+    { cardName: 'card_13', cardValue: '13' },
+    { cardName: 'card_20', cardValue: '20' },
+    { cardName: 'card_40', cardValue: '40' },
+    { cardName: 'card_100', cardValue: '100' },
+    { cardName: 'card_pause', cardValue: '☕' },
+    { cardName: 'card_unkn', cardValue: '❓' },
+  ];
